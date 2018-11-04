35e47f6ce3e61fdc39ce2252ac56f72e6619a93b
Rasmus Thøgersen <rasmusthog@gmail.com>
2018-11-04 02:45:00 +0100

    Refactor

--- import_tools/outcar.py
+++ import_tools/outcar.py
@@ -2,16 +2,89 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import poscar as pc
+
+def load_outcar(path):
+    """ Opens OUTCAR file from 'path' and returns IOWrapper and number of lines """
+
+    ### READ OUTCAR FILE
+    # Open OUTCAR-file
+    outcar = open(path, "r")
+
+    # Count number of lines in OUTCAR-file
+    number_of_lines = 0
+
+    for line in outcar:
+        number_of_lines += 1
+
+    return outcar, number_of_lines
+
+
+
+def get_charge_magnetisation(outcar, number_of_lines, poscar_path):
+
+    # Open POSCAR file and extract dictionary and list of elements (type, order and number)
+    poscar = pc.load_poscar(poscar_path)
+    elements_dict, elements_list = pc.get_elements(poscar)
+
+    # Count number of ions in POSCAR
+    NIONS = 0
+
+    for element in elements_dict:
+        NIONS += int(elements_dict[element])
+
+    # Reset IOWrapper to start 1000 lines above tail. (1000 is arbitrary choice to make sure it is
+    # above the start, but not too much so it's not very inefficient)
+    outcar.seek(number_of_lines - 1000)
+
+    # Create list of only relevant information (after 'reached required accuracy')
+    found = False
+    outcar_list = []
+
+
+    for line in outcar:
+        # Check if 'reached required accuracy' is in line
+        if 'reached required accuracy' in line:
+            found = True
+
+        # If the above line is found, start appending rows to 'outcar_list'
+        if found == True:
+            outcar_list.append(line)
+
+    outcar.close()
+
+
+    # Add total charge of ions to array 'total_charge'
+    total_charge = []
+
+    for i in range(10,NIONS+10):
+        total_charge.append(outcar_list[i].split())
+
+
+    # Add 'total_charge' to pandas dataframe charge_df
+    charge_df = pd.DataFrame(total_charge, columns=["ion", "s", "p", "d", "tot"])
+    charge_df.set_index("ion", inplace=True)
+    charge_df = charge_df.astype(float)
+
+    ## Add magnetisation of ions to array 'magnetisation'
+    magnetisation = []
+
+    for i in range(NIONS+19,(2*NIONS)+19):
+        magnetisation.append(outcar_list[i].split())
+
+    magnetisation_df = pd.DataFrame(magnetisation, columns=["ion", "s", "p", "d", "tot"])
+    magnetisation_df.set_index("ion", inplace=True)
+    magnetisation_df = magnetisation_df.astype(float)
+
+
+    return charge_df, magnetisation_df
 
 
 
 
 
 
-
-
-
-def get_charge(charge_df, orbital='tot', element=0, avg=True):
+def calc_charge(charge_df, orbital='tot', element=0, avg=True):
     """ Returns the charge for a given element.
 
     Keyword arguments:
@@ -37,3 +110,6 @@ def get_charge(charge_df, orbital='tot', element=0, avg=True):
         charge = charge_df[orbital].sum()
     else:
         charge = charge_df[orbital].iloc[charge_df['element'] == elements_list[element-1]]
+
+
+    print(charge_df["d"].iloc[1:8].sum())
