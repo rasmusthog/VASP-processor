069fbdff559b610e7bbfcc27f69a31ac8c9acc82
Rasmus Thøgersen <rasmusthog@gmail.com>
2019-01-05 13:50:56 +0100

    Clean up

---
+++ import_tools/doscar_old.py
@@ -0,0 +1,12 @@
+## Here read_DOSCAR_sp.py and read_DOSCAR.py will be refactored as functions
+
+def load_doscar(path):
+    """ Loads POSCAR file into an array of lists """
+
+    return null
+
+
+def plot_dos(dos_df, spin_polarised=False):
+
+
+    return null
